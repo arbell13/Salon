0b7d17ed2713865947307deef01670ee022dd2c7
arbell13 <floresas@students.nu-baliwag.edu.ph>
2023-04-24 00:28:48 +0800

    change email

--- salon.php
+++ salon.php
@@ -1,43 +1,37 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
- 
-require 'PHPMailer/src/Exception.php';
-require 'PHPMailer/src/PHPMailer.php';
-require 'PHPMailer/src/SMTP.php';
- 
-if(isset($_POST["send"])){
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $mail = new PHPMailer(true);
- 
-    $mail->isSMTP();
-    $mail->Host = "smtp.hostinger.com";
-    $mail->SMTPAuth = true;
-    $mail->Username = "contact@lashesbyabie.shop";
-    $mail->Password = "Arabella@15";
-    $mail->SMTPSecure = "tls";
-    $mail->Port = 587;
- 
-    $mail->setFrom("contact@lashesbyabie.shop");
- 
-   
-    $mail->addAddress('contact@lashesbyabie.shop');
 
- 
+require 'phpmailer/src/Exception.php';
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
+
+if(isset($_POST['submit'])){
+    //POST
+    $email = $_POST['mail'];
+    $name = $_POST['name'];
+    $subject = $_POST['subject'];
+    //$message = $_POST['message'];
+
+    //PHP Mailer Declaration
+    $mail = new PHPMailer(true);
+
+    $mail->isSMTP();
+    $mail->Host = 'smtp.hostinger.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'contact@lashesbyabie.shop';
+    $mail->Password = 'Arabella@15'; //Gmail App Password
+    $mail->SMTPSecure = 'tls';
+    $mail->Port = 587;
+
+    //SETTING Email
+    $mail->setFrom('contact@lashesbyabie.shop'); //Senders Email
+    $mail->addAddress($email); //Receivers Email
     $mail->isHTML(true);
- 
-    $mail->Subject = $_POST["subject"];
-    $mail->Body = "<hr>" . "Good Day " ."<b>$name</b>" . "! " . "<br> Thank you for contacting us. We've received your message and appreciate you reaching out.
-    <br><br>        
-         Regards, <br>
-        <b>Lashes by Abie </b>";
-   $mail->send();
- 
+    $mail->Subject = "Lashes by Abie";
+    $mail->Body = "<hr>" . "Good Day " ."<b>$name</b>" . "! " . "<br>  Hi, thanks for contacting us. We've received your message and appreciate you reaching out.
+    ";
     $mail->send();
- 
-    echo "Feedback Successful!";
- 
- 
+    header('Location: index.php');
 }
 ?>
