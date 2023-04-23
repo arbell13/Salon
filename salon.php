51a92481e70fc6311ee9f30dd20a32d71cc1aaf4
arbell13 <floresas@students.nu-baliwag.edu.ph>
2023-04-24 00:08:53 +0800

    email

--- salon.php
+++ salon.php
@@ -7,26 +7,32 @@ require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
  
 if(isset($_POST["send"])){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
     $mail = new PHPMailer(true);
  
     $mail->isSMTP();
-    $mail->Host = "smtp.gmail.com";
+    $mail->Host = "smtp.hostinger.com";
     $mail->SMTPAuth = true;
-    $mail->Username = "arabellaflores92@gmail.com";
-    $mail->Password = "vavxipiqnmzjpfky";
-    $mail->SMTPSecure = "ssl";
-    $mail->Port = 465;
+    $mail->Username = "contact@lashesbyabie.shop";
+    $mail->Password = "Arabella@15";
+    $mail->SMTPSecure = "tls";
+    $mail->Port = 587;
  
-    $mail->setFrom("arabellaflores92@gmail.com");
+    $mail->setFrom("contact@lashesbyabie.shop");
  
    
-    $mail->addAddress('santosnicholeee@gmail.com');
+    $mail->addAddress('contact@lashesbyabie.shop');
 
  
     $mail->isHTML(true);
  
     $mail->Subject = $_POST["subject"];
-    $mail->Body = $_POST["message"];
+    $mail->Body = "<hr>" . "Good Day " ."<b>$name</b>" . "! " . "<br> Thank you for contacting us. We've received your message and appreciate you reaching out.
+    <br><br>        
+         Regards, <br>
+        <b>Lashes by Abie </b>";
+   $mail->send();
  
     $mail->send();
  
